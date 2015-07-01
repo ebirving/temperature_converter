4af030701e25a1b9252d3309aa581d2562867891
Erica Irving <contact.eirving@gmail.com>
2015-07-01 16:05:19 -0400

    rewrote part 2 using push

--- temp_converter.js
+++ temp_converter.js
@@ -1,45 +1,41 @@
-// Set this variable to a starting Fahrenheit temperature...
-var fahrenheit = 32;
 
-// Enter conversion code below...
+var fahrenheit = 32;
 var fahrenheitToCelsius = (fahrenheit - 32)/1.8;
 var fahrenheitToKelvin = (fahrenheit + 459.67)/1.8;
 
-var fahrenheitTemps = [fahrenheit, fahrenheitToCelsius, fahrenheitToKelvin];
-// Enter console.log statements below...
-//console.log("Farenheit: " + fahrenheit + " °F");
-//console.log("Celsius: " + fahrenheitToCelsius + " °C");
-//console.log("Kelvin: " + fahrenheitToKelvin + " °K");
+var fahrenheitTemps = [];
+fahrenheitTemps.push(fahrenheit);
+fahrenheitTemps.push(fahrenheitToCelsius);
+fahrenheitTemps.push(fahrenheitToKelvin);
+
 console.log("Farenheit: " + fahrenheitTemps[0] + " °F");
 console.log("Celsius: " + fahrenheitTemps[1] + " °C");
 console.log("Kelvin: " + fahrenheitTemps[2] + " °K");
 console.log("\n");
-// Repeat the above for Celsius and Kelvin...
-var celsius = 0;
 
+var celsius = 0;
 var celsiusToFahrenheit = celsius * 1.8 + 32;
 var celsiusToKelvin = celsius + 273.15;
 
-var celsiusTemps = [celsius, celsiusToFahrenheit, celsiusToKelvin];
+var celsiusTemps = [];
+celsiusTemps.push(celsius);
+celsiusTemps.push(celsiusToFahrenheit);
+celsiusTemps.push(celsiusToKelvin);
 
-//console.log("Celsius: " + celsius + " °C");
-//console.log("Farenheit: " + celsiusToFahrenheit + " °F");
-//console.log("Kelvin: " + celsiusToKelvin + " °K");
 console.log("Celsius: " + celsiusTemps[0] + " °C");
 console.log("Farenheit: " + celsiusTemps[1] + " °F");
 console.log("Kelvin: " + celsiusTemps[2] + " °K");
 console.log("\n");
 
 var kelvin = 273.15;
-
 var kelvinToFahrenheit = kelvin * 1.8 - 459.67;
 var kelvinToCelsius = kelvin - 273.15;
 
-var kelvinTemps = [kelvin, kelvinToFahrenheit, kelvinToCelsius];
+var kelvinTemps = [];
+kelvinTemps.push(kelvin);
+kelvinTemps.push(kelvinToFahrenheit);
+kelvinTemps.push(kelvinToCelsius);
 
-//console.log("Kelvin: " + kelvin + " °K");
-//console.log("Farenheit: " + kelvinToFahrenheit.toFixed(2) + " °F");
-//console.log("Celsius: " + kelvinToCelsius + " °C");
 console.log("Kelvin: " + kelvinTemps[0] + " °K");
 console.log("Farenheit: " + kelvinTemps[1].toFixed(2) + " °F");
 console.log("Celsius: " + kelvinTemps[2] + " °C");
